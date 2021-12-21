a06e7155dcd76e8d4a5ba502f7100594d4ca6396
robincao <robincao@tencent.com>
2021-12-21 17:25:18 +0800

    add espresso demo test

--- app/src/androidTest/java/com/tencent/wetestdemo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/tencent/wetestdemo/ExampleInstrumentedTest.java
@@ -1,11 +1,26 @@
 package com.tencent.wetestdemo;
 
 import android.content.Context;
+
+import androidx.lifecycle.Lifecycle;
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,11 +29,51 @@ import org.junit.runner.RunWith;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    public void login(){
+        onView(withId(R.id.username))
+                .perform(typeText("wetest"), closeSoftKeyboard());
+        onView(withId(R.id.password))
+                .perform(typeText("wetest"), closeSoftKeyboard());
+        onView(withId(R.id.login)).perform(click());
+    }
+
+    @Before
+    public void launchActivity() {
+        ActivityScenario<LoginActivity> scenario = ActivityScenario.launch(LoginActivity.class);
+        scenario.moveToState(Lifecycle.State.RESUMED);
+    }
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         org.junit.Assert.assertEquals("com.tencent.wetestdemo",
                 appContext.getPackageName());
+    }
+
+    @Test
+    public void runLoginSuccessTest()  {
+        login();
+        onView(withId(R.id.submitbtn)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void runLoginFailedTest()  {
+        onView(withId(R.id.login)).perform(click());
+        onView(withText("Login Failed")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void runSelectItemTest() {
+        login();
+        onView(withText("Item0")).perform(click());
+        onView(withId(R.id.submitbtn)).perform(click());
+        onView(withId(R.id.select_items)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void runFailedTest()  {
+       assert false;
     }
 }
