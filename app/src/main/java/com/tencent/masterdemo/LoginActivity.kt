880765ce0b2ebc870a9c517a65f6364ae29ce44c
kiwimchen <kiwimchen@tencent.com>
2022-02-28 19:57:27 +0800

    add login activity

---
+++ app/src/main/java/com/tencent/masterdemo/LoginActivity.kt
@@ -0,0 +1,17 @@
+package com.tencent.masterdemo
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        val loginButton = findViewById<Button>(R.id.login)
+        loginButton.setOnClickListener {
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+        }
+    }
+}
