f6714882a073e2e0a5efbd198aa77df1a206032e
kiwimchen <kiwimchen@tencent.com>
2022-02-28 19:27:07 +0800

    init

---
+++ app/src/main/java/com/tencent/masterdemo/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.tencent.masterdemo.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
