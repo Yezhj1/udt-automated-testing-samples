39bdb764ace175b8326c9aec1dbab5170d24fd5e
kiwimchen <kiwimchen@tencent.com>
2022-03-01 12:02:19 +0800

    add black test

---
+++ app/src/main/java/com/tencent/masterdemo/BlackActivity.kt
@@ -0,0 +1,42 @@
+package com.tencent.masterdemo
+
+import android.app.Activity
+import android.app.ActionBar
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+
+class BlackActivity : AppCompatActivity() {
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_black)
+
+        statusBarHide(this)
+    }
+
+    /**
+     * 设置Activity的statusBar隐藏
+     * @param activity
+     */
+    fun statusBarHide(activity: Activity) {
+        // 代表 5.0 及以上
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val decorView: View = activity.window.decorView
+            val option: Int = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            decorView.setSystemUiVisibility(option)
+            activity.window.statusBarColor = Color.TRANSPARENT
+            val actionBar: ActionBar? = activity.actionBar
+            actionBar?.hide()
+            return
+        }
+
+        // versionCode > 4.4  and versionCode < 5.0
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            activity.window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        }
+    }
+}
--- app/src/main/java/com/tencent/masterdemo/ui/home/HomeFragment.kt
+++ app/src/main/java/com/tencent/masterdemo/ui/home/HomeFragment.kt
@@ -1,15 +1,18 @@
 package com.tencent.masterdemo.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.tencent.masterdemo.BlackActivity
 import com.tencent.masterdemo.R
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), View.OnClickListener {
     private var adapter: NewsAdapter? = null
 
     private lateinit var homeViewModel: HomeViewModel
@@ -20,6 +23,7 @@ class HomeFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_home, container, false)
+        root.findViewById<ImageView>(R.id.banner).setOnClickListener(this)
         return root
     }
 
@@ -35,4 +39,11 @@ class HomeFragment : Fragment() {
         adapter!!.setModel(homeViewModel)
     }
 
+    override fun onClick(view: View?) {
+        when(view?.id) {
+            R.id.banner -> {
+                startActivity(Intent(context, BlackActivity::class.java))
+            }
+        }
+    }
 }
