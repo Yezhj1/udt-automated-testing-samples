c3773f3ddda30b9c8da78af0047343b19ffd3e84
zhaojunye <zhaojunye@tencent.com>
2022-06-13 19:05:22 +0800

    make some adjustments

--- app-android-wetest-demo/test_demo.py
+++ app-android-wetest-demo/test_demo.py
@@ -1,4 +1,26 @@
-import sys
+# import sys
+# import os
+# sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from uitrace.api import *
+import cv2 as cv
 
 
-print('path', sys.path)
+def test_click():
+    init_driver(os_type=OSType.ANDROID, workspace=os.path.dirname(__file__),
+                mode=RunMode.SINGLE, dirver_lib=DriverLib.SCRCPY)
+
+    click([200, 200])
+    tic = time.time()
+    toc = tic
+    while toc - tic < 20:
+        img = get_img()
+        if img is not None:
+            cv.imshow("test_android", img)
+            cv.waitKey(1)
+            toc = time.time()
+
+    stop_driver()
+
+
+if __name__ == '__main__':
+    test_click()
