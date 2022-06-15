350a765ff113ce945dc0c5669fca63431a7505e6
zhaojunye <zhaojunye@tencent.com>
2022-06-15 14:56:10 +0800

    fix: support account input

--- qq-WeA-demo/test_qq_android.py
+++ qq-WeA-demo/test_qq_android.py
@@ -1,8 +1,19 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from uitrace.api import *
 from Xpath import *
+import argparse
 
 
-def test_qq():
+def arg():
+    parse = argparse.ArgumentParser()
+    parse.add_argument('--qq', typr=str, default='')
+    parse.add_argument('-password', type=str, default='')
+    return parse.parse_args()
+
+
+def test_qq(arg):
     init_driver(os_type=OSType.ANDROID, workspace=os.path.dirname(__file__), mode=RunMode.SINGLE,
                 driver_lib=DriverLib.SCRCPY)
     start_app('com.tencent.mobileqq',
@@ -10,13 +21,14 @@ def test_qq():
               clear_app=True, clear_account=False)
     click(login, by=DriverType.UI, timeout=20)
     click(account, by=DriverType.UI, timeout=20)
-    input_text('qq number') # input a qq number
+    input_text(arg.qq) # input a qq number
     click(password, by=DriverType.UI, timeout=20)
-    input_text('password') # qq password
+    input_text(arg.password) # qq password
     click(confirm, by=DriverType.UI, timeout=20)
 
     stop_driver()
 
 
 if __name__ == '__main__':
-    test_qq()
+    info = arg()
+    test_qq(info)
