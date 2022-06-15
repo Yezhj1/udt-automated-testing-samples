ec4ac9ee89b22e66bd081e21f43478d5a46b1fff
zhaojunye <zhaojunye@tencent.com>
2022-06-15 15:14:08 +0800

    build: create a init file

---
+++ camera-wetest-demo/init.py
@@ -0,0 +1,3 @@
+Android_camera = 'com.huawei.camera'
+Android_shoot = '//android.widget.RelativeLayout[2]/android.widget.LinearLayout/android.widget.LinearLayout[2]'
+Android_switch_camera = '//android.widget.LinearLayout[@resource-id="com.huawei.camera:id/lyt_normal_control_bar_switcher_single"]/android.widget.ImageView[@resource-id="com.huawei.camera:id/iv_normal_control_bar_switcher_single"]'
